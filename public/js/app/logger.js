7a398751b4dd2e443696978e83f4e11e61988b0b
Corey Schram <coreyschram@gmail.com>
2013-11-03 00:19:50 -0500

    Working on fixing IE logging issue.

--- public/js/app/logger.js
+++ public/js/app/logger.js
@@ -1,20 +1,50 @@
-define({
+define(function () {
 
-    currentGroup: null,
-
-    log: function (group, message) {
-        if (message) {
-            if (group !== this.currentGroup) {
-                console.groupEnd();
-                console.group(group);
-                this.currentGroup = group;
+    // Shim console groups
+    var groups = [];
+    if ( typeof console.group !== 'function' ) {
+        console.group = function ( group ) {
+            if ( groups[ groups.length - 1 ] !== group ) {
+                groups.push( group );
             }
-            console.log(message);
-        } else {
-            message = group;
-            console.groupEnd();
-            console.log(message);
-        }
+        };
+
+        console.groupEnd = function ( group ) {
+            if ( groups[ groups.length - 1 ] === group ) {
+                groups.pop();
+            }
+        };
+
+        var log = console.log || function () {};
+        console.log = function () {
+            var msg = [
+                '[',
+                groups.join(']['),
+                ']: ',
+                [].prototype.join.call( arguments, ' ' )
+            ].join('');
+            log( msg );
+        };
     }
+
+    return {
+
+        currentGroup : null,
+
+        log : function ( group, message ) {
+            if ( message ) {
+                if ( group !== this.currentGroup ) {
+                    console.groupEnd();
+                    console.group( group );
+                    this.currentGroup = group;
+                }
+                console.log( message );
+            } else {
+                message = group;
+                console.groupEnd();
+                console.log( message );
+            }
+        }
+    };
 
 });
